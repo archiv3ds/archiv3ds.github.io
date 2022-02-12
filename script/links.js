397600c3b41fae4e697ef0cf4ee1175d4d564f81
archiv3ds <archiv3dscontact@gmail.com>
2022-02-12 14:26:28 +0300

    inital commit

--- script/links.js
+++ script/links.js
@@ -1,7 +1,7 @@
 var  links = {
-    1: "https://www.artstation.com/urico"
-    2: "https://www.artstation.com/zeen"
-    3: "https://www.artstation.com/yuzhibolaoshu"
-    4: "https://www.artstation.com/yileina"
-    5: "https://www.artstation.com/artxiaofei"
+    1071: "https://www.artstation.com/urico"
+    1072: "https://www.artstation.com/zeen"
+    1073: "https://www.artstation.com/yuzhibolaoshu"
+    1074: "https://www.artstation.com/yileina"
+    1075: "https://www.artstation.com/artxiaofei"
 };
